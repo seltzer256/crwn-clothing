d3c378424fb5ba06ad6a44a6b2e3e0b0aaa01035
AftershockREYESN <jorge@aftershock.agency>
2022-02-15 15:40:26 -0500

    Navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import { Route } from "react-router";
 
 function App() {
     return (
         <div className="">
             <HomePage />
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/topics" component={HomePage} />
         </div>
     );
 }
