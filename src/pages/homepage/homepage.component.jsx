996c8ed60ee1b3a610b1af64ec16224f2800ca2d
henryaftershock <henry@aftershock.agency>
2022-02-24 16:35:49 -0500

    Homepage error

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -3,7 +3,6 @@ import Directory from "../../components/directory/directory.component";
 import HomePageContainer from "./homepage.styles";
 
 const HomePage = () => {
-    throw new Error("Error");
     return (
         <HomePageContainer>
             <Directory />
